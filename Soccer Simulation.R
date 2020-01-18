b3c7b8e70f3432a3ef69a267a579851cf1904cff
sonolio <54865552+sonolio@users.noreply.github.com>
2020-01-17 21:10:49 -0700

    Update Soccer Simulation.R

--- Soccer Simulation.R
+++ Soccer Simulation.R
@@ -1,8 +1,8 @@
 myData <- read.csv("Downloads/Soccer Simulation/Soccer Teams Rating - Data.csv" , header = TRUE)
 head(myData)
 
-homeTeam = 'Villa'
-awayTeam = 'City'
+homeTeam = 'Arsenal'
+awayTeam = 'Sheffield_Utd'
 
 outputHomeTeam = c()
 outputAwayTeam = c()
@@ -15,9 +15,9 @@ countGoalsScoredByAwayTeam = 0
 for (x in 1:numberOfTimesToRunSim) {
   for (i in 1:length(myData$team)) {
     if(myData$team[i]==homeTeam){
-      print("The home team Southampton took the shot")
+      print("The home team Arsenal took the shot")
     }else if (myData$team[i]==awayTeam){
-      print("The away team Liverpool took the shot")
+      print("The away team Sheffield_Utd took the shot")
     }
   }
 } # end of numberOfTimesToRunSimLoop
