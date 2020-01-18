4fe26684b047cfec5daa0c04a5f49018ed74cf4f
sonolio <54865552+sonolio@users.noreply.github.com>
2020-01-17 20:50:12 -0700

    Update Possion Dist.R

--- Possion Dist.R
+++ Possion Dist.R
@@ -4,7 +4,12 @@ head(data)
 n <- 0:6
 #Arsenal Lambda = 1.355611
 Arsenal <-dpois(n, lambda = 1.355700)*100
-#odds <- ppois(n, lambda = 1.355611, lower = FALSE)*100
+#Sheffield_Utd Lambda = 1.013155
 Sheffield_Utd <-dpois(n, lambda = 1.013155)*100
 
 data.frame(Arsenal,Sheffield_Utd)
+
+#probability of Arsenal and Sheffield_Utd scoring 4 or less goals  
+#Arsenal_4<- ppois(4, lambda = 1.355611, lower = FALSE)*100
+#Sheffield_Utd_4 <- ppois(4, lambda = 1.013155, lower = FALSE)*100
+#data.frame(Arsenal_4,Sheffield_Utd_4)
